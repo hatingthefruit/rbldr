d1afe0fcb0a8baf6bf21dc5a306994a6d399e97f
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2021-07-28 20:15:15 -0500

    added models

--- cmd/root.go
+++ cmd/root.go
@@ -53,7 +53,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.rbldr.git.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.rbldr.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -73,7 +73,7 @@ func initConfig() {
 		// Search config in home directory with name ".rbldr.git" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".rbldr.git")
+		viper.SetConfigName(".rbldr")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
