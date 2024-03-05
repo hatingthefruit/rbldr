944702e9b4a06680f248bf02215706870baa7cc4
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2024-03-05 11:19:15 -0800

    Finish resume template and fix character escaping

--- util/templateEngine.go
+++ util/templateEngine.go
@@ -6,7 +6,7 @@ import (
 )
 
 func FillTemplate(resume Resume, resumeTemplate string, outputWriter io.Writer) error {
-	t := template.Must(template.New("resumeTemplate").Parse(resumeTemplate))
+	t := template.Must(template.New("resumeTemplate").Funcs(template.FuncMap{"escapeCharacters": EscapeCharacters}).Parse(resumeTemplate))
 
 	return t.Execute(outputWriter, resume)
 }
